ca6431b0a332b8372d8e22543050c3841623acf0
João Pedro Carpanezi dos Santos <jpcarpanezi@gmail.com>
2021-07-12 11:38:57 -0300

    First commit

---
+++ src/main/java/com/dooc/health/UnifiedHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dooc.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedHealthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiedHealthSystemApplication.class, args);
+	}
+
+}
