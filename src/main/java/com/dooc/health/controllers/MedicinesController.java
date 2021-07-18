72330a885a336df7ef412e22cd9dd48885bd9069
Aldemir.neto <humbertoaldemir@gmail.com>
2021-07-18 15:24:36 -0300

    Feito Medicines

--- src/main/java/com/dooc/health/controllers/MedicinesController.java
+++ src/main/java/com/dooc/health/controllers/MedicinesController.java
@@ -2,8 +2,10 @@ package com.dooc.health.controllers;
 
 import com.dooc.health.infrastructure.MedicinesRepository;
 import com.dooc.health.models.Medicines;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientResponseException;
 
 @RestController
 @RequestMapping("/medicines")
@@ -14,20 +16,58 @@ public class MedicinesController {
 		this.medicinesRepository = medicinesRepository;
 	}
 
-	/* retorna todos os remedios do repositorio  */
+	@PostMapping
+	public @ResponseStatus ResponseEntity<Medicines> insertMedicines(@RequestBody Medicines medicines) throws RestClientResponseException{
+		Medicines insertMedicines = new Medicines();
+		insertMedicines.setDrugName(medicines.getDrugName());
+		insertMedicines.setActiveIngredient(medicines.getActiveIngredient());
+		insertMedicines.setFormRoute(medicines.getFormRoute());
+		insertMedicines.setCompany(medicines.getCompany());
 
-	/* obter um remedio especifico pelo seu ID */
+		medicinesRepository.save(insertMedicines);
+		return  new ResponseEntity<Medicines>(medicines, HttpStatus.CREATED);
+
+	}
 
 
-	/* obter um remedio pelo seu Ingrediente Ativo */
+	@GetMapping("/medicamento")
+	public @ResponseStatus ResponseEntity<Medicines> getPersonByParam(@RequestParam(defaultValue = "drugName") String selectBy, @RequestParam(required = false) String drugName, @RequestParam(required = false) String activeIngredient) throws RestClientResponseException {
+		Medicines medicines = null;
+		if(selectBy.equals("drugName") && drugName != null) {
+			medicines = medicinesRepository.getMedicinesByDrugName(drugName);
+		} else if(selectBy.equals("activeIngredient") && activeIngredient!= null) {
+			medicines = medicinesRepository.getMedicinesByActiveIngredientLike('%'+activeIngredient+'%');
+		}
+
+		if (medicines == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Medicines>(medicines, HttpStatus.OK);
+	}
+
+	@PutMapping
+	public @ResponseStatus ResponseEntity<Medicines> updateMedicines(@RequestBody Medicines medicines, @RequestParam(defaultValue = "drugName") String updateMethod, @RequestParam(required = false) String drugName, @RequestParam(required = false) String activeIngredient) throws RestClientResponseException {
+		Medicines updateMedicines = null;
+		if (updateMethod.equals("drugName")) {
+			updateMedicines = medicinesRepository.getMedicinesByDrugName(drugName);
+		} else if(updateMethod.equals("activeIngredient")) {
+			updateMedicines = medicinesRepository.getMedicinesByActiveIngredientLike(activeIngredient);
+		}
+
+		if (updateMedicines == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		updateMedicines.setDrugName(medicines.getDrugName());
+		updateMedicines.setActiveIngredient(medicines.getActiveIngredient());
+		updateMedicines.setFormRoute(medicines.getFormRoute());
+		updateMedicines.setCompany(medicines.getCompany());
 
 
-/*@getmapping ("/nometabela)- selecionar tudo
-	  @getmapping("/nometabela/{nomecoluna}") - obter especifico
-	  @deletemapping - Deletar
-	  @postmapping - Inserir
-	  @putmapping - Atualizar
-	 */
+		medicinesRepository.save(updateMedicines);
+		return new ResponseEntity<Medicines>(updateMedicines, HttpStatus.OK);
+	}
 
 }
 
