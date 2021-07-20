0dba02a1b6064a5c362f11bb3aa54d4ad938ad7b
Murilo Azevedo Jacon <murilo.ajac@gmail.com>
2021-07-20 01:26:29 -0300

    Added new prescriptions get

--- src/main/java/com/dooc/health/infrastructure/PrescriptionsRepository.java
+++ src/main/java/com/dooc/health/infrastructure/PrescriptionsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PrescriptionsRepository extends CrudRepository<Prescriptions, Integer> {
+    Iterable<Prescriptions> getPrescriptionsByIdConsultation(int id);
 }
